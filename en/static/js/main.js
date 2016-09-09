7d588ee4d692c40511aec5a571ab8c49c558f827
riverleo <iam.riverleo@gmail.com>
2016-09-09 11:21:44 +0900

    Eng Structure

---
+++ en/static/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+    'use strict';
+
+    angular.module('main', ['ui.bootstrap', 'ngResource'])
+        .directive('main', function () {
+            return {
+                restrict: 'E',
+                controller: function ($scope, $location) {
+                    $scope.tpls = {};
+                    $scope.tpls.header = 'template/header.tpl.html';
+                    $scope.tpls.footer = 'template/footer.tpl.html';
+
+                    $scope.isProduction = ($location.host() == 'd3jubilee.com')
+                }
+            }
+        })
+        .controller('HomeController', function ($scope, $resource) {
+            $scope.features = $resource('static/json/feature.json').get();
+        })
+        .controller('PortfolioController', function ($scope, $resource) {
+            $scope.category = $resource('static/json/category.json').get();
+        })
+}(jQuery));
